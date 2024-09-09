f111ee41b5596a9e511d98715fc1013a4ef89686
trihung987 <51873366+trihung987@users.noreply.github.com>
2024-09-10 02:50:12 +0700

    update ~

--- src/main/java/me/trihung/controllers/WaitingController.java
+++ src/main/java/me/trihung/controllers/WaitingController.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/waiting")
@@ -15,18 +17,42 @@ public class WaitingController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		    resp.setContentType("text/html");
-		    PrintWriter out = resp.getWriter();
-		    // HTML page
-		    out.println("<!DOCTYPE html>");
-		    out.println("<html>");
-		    out.println("<head>");
-		    out.println("<title>Hello World</title>");
-		    out.println("</head>");
-		    out.println("<body>");
-		    out.println("<h1>Xin chào!</h1>");
-		    out.println("</body>");
-		    out.println("</html>");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		// HTML page
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Xin chào!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		HttpSession session = req.getSession(false);
+		//check if already cookie or not
+		if (session != null && session.getAttribute("account") != null) {
+			return;
+		}
+		// Check cookie
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("username")) {
+					session = req.getSession(true);
+					session.setAttribute("username", cookie.getValue());
+					return;
+				}
+			}
+			
+		}
+		// go back home page
+		goHomePage(req, resp);
+
+	}
+
+	public void goHomePage(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		req.getRequestDispatcher("home.jsp").forward(req, resp);
 
 	}
 }
